377d3a6a95d1e19861821551d187cade8bf4f65e
jangwoosol <88824569+jangwoosol@users.noreply.github.com>
2022-09-13 10:07:40 +0900

    Update 숫자 카드 2.py

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -11,3 +11,11 @@ for n in N:
         dic[n] = 1
 
 print(' '.join(str(dic[i]) if i in dic else '0' for i in M))
+
+''' code는 같지만 런타임에러
+for i in M:
+  if i in dic:
+    print(' '.join(str(dic[i])),end=' ')
+  else:
+    print('0', end=' ') 
+''' 
