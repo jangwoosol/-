54c6fb22dbbecca3fc2e74c4b33c51139b787ec5
jangwoosol <88824569+jangwoosol@users.noreply.github.com>
2022-11-07 14:57:43 +0900

    Create 곱하기 혹은 더하기 문제.py

---
+++ 알고리즘으로 생각하기/그리디알고리즘/곱하기 혹은 더하기 문제/곱하기 혹은 더하기 문제.py
@@ -0,0 +1,12 @@
+data=input()
+# 첫번째 문자를 숫자로 변경해서 대입
+result=int(data[0])
+for i in range(1,len(data)):
+  # 두 수 중 하나라도 1이하인 경우 더하기 수행
+  num=int(data[i])
+  if num<=1 or result<=1:
+    result+=num
+  else:
+    result*=num
+
+print(result)
