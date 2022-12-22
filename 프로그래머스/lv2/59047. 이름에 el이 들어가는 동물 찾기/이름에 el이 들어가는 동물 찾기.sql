489601c54410013d76f8a77c27af01654d758792
jangwoosol <88824569+jangwoosol@users.noreply.github.com>
2022-12-22 11:42:42 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,5 +1,6 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME 
-FROM ANIMAL_INS 
-WHERE NAME LIKE "%EL%" AND ANIMAL_TYPE = "Dog" 
-ORDER BY NAME
+# %가 앞뒤로 있으면 어딘가에 EL이 들어있음 된다.
+SELECT ANIMAL_ID,	NAME
+from ANIMAL_INS
+where 	ANIMAL_TYPE='Dog' and NAME like '%EL%'
+order by name
