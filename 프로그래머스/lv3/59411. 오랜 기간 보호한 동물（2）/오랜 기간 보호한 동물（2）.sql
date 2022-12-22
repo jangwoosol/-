b0df8eec4f9a141524b3a04897b6eebb4e3946df
jangwoosol <88824569+jangwoosol@users.noreply.github.com>
2022-12-22 11:55:12 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,6 +1,5 @@
 -- 코드를 입력하세요
-SELECT a.ANIMAL_ID, a.NAME
-from animal_ins a, animal_outs b
-where a.animal_id = b.animal_id
-order by datediff(a.datetime,b.datetime)
+SELECT b.ANIMAL_ID,	b.NAME
+from animal_ins a right join animal_outs b on a.ANIMAL_ID=b.ANIMAL_ID
+order by b.datetime-a.datetime DESC
 limit 2
