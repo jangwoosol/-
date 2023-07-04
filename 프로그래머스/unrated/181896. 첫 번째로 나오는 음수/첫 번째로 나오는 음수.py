c097386c791f7c5cfc56e28b2f7304028519b255
jangwoosol <88824569+jangwoosol@users.noreply.github.com>
2023-07-04 16:40:13 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,9 +1,5 @@
 def solution(num_list):
-    a=[]
-    for i in num_list:
-        if i<0:
-            a.append(num_list.index(i))
-    if len(a)==0:
-        return -1
-    else:
-        return a[0]
+    for i in range(len(num_list)):
+        if num_list[i]<0:
+            return i
+    return -1
