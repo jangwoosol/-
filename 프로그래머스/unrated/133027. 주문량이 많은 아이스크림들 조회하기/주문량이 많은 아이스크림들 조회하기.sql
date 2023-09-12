fcd5e8549a9fbda60f7cd0b15a3429faaa4b9fd1
jangwoosol <88824569+jangwoosol@users.noreply.github.com>
2023-09-12 10:06:25 +0900

    [unrated] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,6 +1,8 @@
 -- 코드를 입력하세요
-SELECT a.flavor
-from first_half as a join (select flavor, sum(total_order) as total_order from july group by flavor) as b 
-on a.flavor=b.flavor
-order by (a.total_order+ b.total_order) desc
+#같은 맛의 아이스크림이라도 다른 출하 번호를 갖게 된다.
+select a.flavor
+from (SELECT shipment_id, flavor, sum(total_order) as total
+from july 
+group by flavor) as a join (SELECT shipment_id, flavor, sum(total_order) as total from FIRST_HALF group by flavor) as b on a.flavor=b.flavor 
+order by a.total+b.total desc
 limit 3
