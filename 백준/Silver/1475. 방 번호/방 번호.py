62a87abbd6c400d8265685eb67f69c36da91c47c
jangwoosol <88824569+jangwoosol@users.noreply.github.com>
2023-11-19 23:12:07 +0900

    [Silver V] Title: 방 번호, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,9 +1,9 @@
 n = input()
 cnt = [0] * 9
-for x in n:
-    ind = int(x)
-    if ind == 9:
-        ind = 6
-    cnt[ind] += 1
-cnt[6] = (cnt[6]+1) // 2
+for i in n:
+    k=int(i)
+    if k==9:
+        k=6
+    cnt[k]+=1
+cnt[6]=(cnt[6]+1)//2 #9도 6으로 취급해서 넣었으니 개수 셀 때 반 나눔!
 print(max(cnt))
